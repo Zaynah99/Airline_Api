6f0cbdb7a1b39d30d4eab0045605a091853fc526
Zaynah <zaynah.sadiq@hotmail.com>
2023-04-20 17:40:07 +0100

    added DTO

--- src/main/java/com/example/Airline_Api/components/DataLoader.java
+++ src/main/java/com/example/Airline_Api/components/DataLoader.java
@@ -1,7 +1,30 @@
 package com.example.Airline_Api.components;
 
+import com.example.Airline_Api.repositories.FlightRepository;
+import com.example.Airline_Api.repositories.PassengerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.stereotype.Component;
+
+@Component
 public class DataLoader {
 
-    
+    @Autowired
+    FlightRepository flightRepository;
+
+    @Autowired
+    PassengerRepository passengerRepository;
+
+    public DataLoader(){
+    }
+
+//    @Override
+//    public void run(ApplicationArguments args) throws Exception {
+//
+//
+//
+//
+//    }
+
 
 }
---
+++ src/main/java/com/example/Airline_Api/models/FlightDTO.java
@@ -0,0 +1,69 @@
+package com.example.Airline_Api.models;
+
+import java.util.List;
+
+//
+public class FlightDTO {//POJO
+
+//    PROPERTIES
+    private String destination;
+    private int capacity;
+    private double departureDate;
+    private double departureTime;
+    private List<Long> passengerIds;
+
+//    CONSTRUCTOR
+    public FlightDTO (String destination, int capacity, double departureDate, double departureTime, List passengerIds){
+        this.destination = destination;
+        this.capacity = capacity;
+        this.departureDate = departureDate;
+        this.departureTime = departureTime;
+        this.passengerIds = passengerIds;
+    }
+
+//    DEFAULT CONSTRUCTOR
+    public FlightDTO(){
+
+    }
+
+//    GETTERS AND SETTERS
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public double getDepartureDate() {
+        return departureDate;
+    }
+
+    public void setDepartureDate(double departureDate) {
+        this.departureDate = departureDate;
+    }
+
+    public double getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(double departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public List<Long> getPassengerIds() {
+        return passengerIds;
+    }
+
+    public void setPassengerIds(List<Long> passengerIds) {
+        this.passengerIds = passengerIds;
+    }
+}
