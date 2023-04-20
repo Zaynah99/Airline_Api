6e0d2c6ecad62a89151c479d38dc27a59db167d5
Zaynah <zaynah.sadiq@hotmail.com>
2023-04-20 16:10:37 +0100

    added pojo classes

--- src/main/java/com/example/Airline_Api/models/Flight.java
+++ src/main/java/com/example/Airline_Api/models/Flight.java
@@ -1,4 +1,75 @@
 package com.example.Airline_Api.models;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
 
-public class Flight {
+@Entity(name = "flights") //creates table
+public class Flight { //POJO
+
+    @JsonIgnoreProperties({"flights"})
+    @ManyToMany(mappedBy = "flights")
+
+//    PROPERTIES
+    @Id // creates unique identifier
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column
+    private String destination;
+
+    @Column
+    private int capacity;
+
+    @Column (name = "departure_date")
+    private double departureDate;
+
+    @Column (name = "departure_time")
+    private double departureTime;
+
+//    CONSTRUCTOR
+    public Flight (String destination, int capacity, double departureDate, double departureTime){
+        this.destination = destination;
+        this.capacity = capacity;
+        this.departureDate = departureDate;
+        this.departureTime = departureTime;
+    }
+
+//    DEFAULT CONSTRUCTOR
+    public Flight(){
+
+    }
+
+
+//    GETTERS AND SETTERS
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public double getDepartureDate() {
+        return departureDate;
+    }
+
+    public void setDepartureDate(double departureDate) {
+        this.departureDate = departureDate;
+    }
+
+    public double getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(double departureTime) {
+        this.departureTime = departureTime;
+    }
 }
--- src/main/java/com/example/Airline_Api/models/Passenger.java
+++ src/main/java/com/example/Airline_Api/models/Passenger.java
@@ -1,7 +1,57 @@
 package com.example.Airline_Api.models;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
 
-public class Passenger {
+@Entity (name = "passengers") //creates table
+public class Passenger {//POJO
+
+    @ManyToMany
+    @JsonIgnoreProperties({"passengers"})
+    @JoinTable(
+            name = "passengers_flights",
+            joinColumns = @JoinColumn(name = "passengers_id"),
+            inverseJoinColumns = @JoinColumn (name = "flights_id")
+
+    )
+
+//  PROPERTIES
+    @Id //creates unique identifier
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private String name;
+
+    @Column
+    private int phoneNumber;
+
+//    JOIN TABLE??
+
+//    CONSTRUCTOR
+    public Passenger(String name, int phoneNumber){
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
 
 
+//    DEFAULT CONSTRUCTOR
+    public Passenger(){
 
+    }
+
+//    GETTERS AND SETTERS
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
